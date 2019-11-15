9f892ce21d7721d6756ddd2d4d36d1b7978fc240
Adnan Alam <adnanzalam@gmail.com>
2019-11-14 21:02:32 -0500

    adding comments

--- src/FileReader.py
+++ src/FileReader.py
@@ -2,9 +2,11 @@ from PatientDemographic import PatientInfoList
 
 
 def read_input(path):
+    # Construct the Patient Demographic list
     patient_list = PatientInfoList()
     with open(path, encoding='utf8') as fp:
         for line in fp:
+            # Add each line read from the input file to the list
             patient_list.add(line)
 
     return patient_list
--- src/PatientDemographic.py
+++ src/PatientDemographic.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class PatientInfo:
     _id: str
@@ -24,15 +25,25 @@ class PatientInfoList:
         self.patient_list = {}
 
     def add(self, line):
+        # Split each line by the comma delimiter
         info = line.split(',')
+
+        # Extract each piece of info, assuming input is valid and remove any potential leading/trailing whitespace
         _id = info[0].strip()
+
+        # Split and extract the first, last, and optional middle name by the '^' delimiter
         f_name, l_name, m_name = self.get_names(info[1])
         sex = info[2].strip()
         dob = info[3].strip()
+
+        # Populate the info object with the extracted information
         patient_info_obj = PatientInfo(_id, f_name, l_name, m_name, sex, dob, line.rstrip())
 
+        # The key for each element in the hash table will be in the format '<last_name>^<first_name>' all lowercase
+        # This will resolve the scenario for grouping patient info with the same name with lower/upper case characters
         name_key = l_name.lower() + "^" + f_name.lower()
 
+        # Add the info object to the hash table given the key where the value will be a list of info objects
         if name_key in self.patient_list:
             self.patient_list[name_key].append(patient_info_obj)
         else:
@@ -43,6 +54,7 @@ class PatientInfoList:
         return name_list[1].strip(), name_list[0].strip(), "" if len(name_list) < 3 else name_list[2].strip()
 
     def print_output(self):
+        # Print the output with each patient info grouped by the patient name as desired
         i = 0
         for patient_info in self.patient_list.values():
             itr_num = str(i) + ":"
@@ -51,6 +63,7 @@ class PatientInfoList:
             for single_info in patient_info:
                 print(single_info.out)
 
+    # Methods below are for the purpose of testing #
     def clear(self):
         self.patient_list.clear()
 
